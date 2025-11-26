4584cdbee59840380e1d0892f6e2370a1be3a5ce
Umarfarook <umarfarookj06@gmail.com>
2025-11-26 18:29:55 +0530

    app form added

--- frontend/src/hooks/useAppForm.ts
+++ frontend/src/hooks/useAppForm.ts
@@ -1,10 +1,11 @@
-import { useForm, UseFormProps, UseFormReturn } from "react-hook-form";
+import { useForm } from "react-hook-form";
+import type { UseFormProps, UseFormReturn, FieldValues } from "react-hook-form";
 
 /**
  * useAppForm - Global wrapper for react-hook-form with consistent config
  * Use this instead of useForm directly for all forms.
  */
-export function useAppForm<T>(options?: UseFormProps<T>): UseFormReturn<T> {
+export function useAppForm<T extends FieldValues>(options?: UseFormProps<T>): UseFormReturn<T> {
     return useForm<T>({
         mode: "onBlur",
         reValidateMode: "onChange",
