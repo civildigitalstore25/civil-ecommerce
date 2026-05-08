7ef61bdd8d444c21e58d78c0fb1a29fd1c7210b7
vidhyasagar70 <vidhyas369@gmail.com>
2026-05-08 15:09:17 +0530

    my orderpage updated

--- frontend/src/components/orders/ProductInfo.tsx
+++ frontend/src/components/orders/ProductInfo.tsx
@@ -1,9 +1,6 @@
+
 import React from "react";
-import { useAdminTheme } from "../../contexts/AdminThemeContext";
-import { useCurrency } from "../../contexts/CurrencyContext";
 import type { IOrder } from "../../api/types/orderTypes";
-import { useProductOrderDownload } from "./productInfo/useProductOrderDownload";
-import { ProductInfoPanel } from "./productInfo/ProductInfoPanel";
 
 interface ProductInfoProps {
   order: IOrder;
@@ -11,25 +8,9 @@ interface ProductInfoProps {
   onViewDetails: () => void;
 }
 
-const ProductInfo: React.FC<ProductInfoProps> = React.memo(
-  ({ order, onBuyAgain: _onBuyAgain, onViewDetails: _onViewDetails }) => {
-    const { colors, theme } = useAdminTheme();
-    const { formatPriceWithSymbol } = useCurrency();
-    const product = order.items[0];
-    const download = useProductOrderDownload(order, product);
 
-    return (
-      <ProductInfoPanel
-        order={order}
-        product={product}
-        colors={colors}
-        theme={theme}
-        formatPriceWithSymbol={formatPriceWithSymbol}
-        download={download}
-      />
-    );
-  },
-);
+// Deprecated: ProductInfo is no longer used. Use ProductInfoPanel directly for each order item.
+const ProductInfo: React.FC<ProductInfoProps> = () => null;
 
 ProductInfo.displayName = "ProductInfo";
 
